38a6e97fb8c38dd668d06f2900bccd75cd8890d7
Vida Owusu <vidakumiwaaowusu@gmail.com>
2024-07-26 12:15:54 +0000

    login has been worked on

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { dbConnection } from "./config/db.js";
 import { userRouter } from "./routes/user_route.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
@@ -8,6 +10,18 @@ dbConnection();
 
 // Use middlewares
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    //cookie: { secure: true }
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+    }),
+  })
+);
+
 
 // Use routes
 app.use(userRouter);
--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { signUp } from "../controllers/user_controller.js";
+import { signUp, token } from "../controllers/user_controller.js";
+import jwt from "jsonwebtoken"
 
 export const userRouter = Router();
 
 userRouter.post("/api/auth/register", signUp);
+
+userRouter.post('/api/auth/login', token)
