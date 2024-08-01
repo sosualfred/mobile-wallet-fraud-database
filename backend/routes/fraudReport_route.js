e5cad351e44f20bac9c17bdceeaf7d732257bf37
Vida Owusu <vidakumiwaaowusu@gmail.com>
2024-08-01 10:25:06 +0000

    update report has been worked on

--- backend/routes/fraudReport_route.js
+++ backend/routes/fraudReport_route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { addFraudReport } from "../controllers/fraudReport_controller.js";
+import { addFraudReport, updateFraudReport } from "../controllers/fraudReport_controller.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 export const fraudReportRouter = Router();
 
 fraudReportRouter.post("/api/fraud/report", isAuthenticated, addFraudReport);
+
+fraudReportRouter.put("/api/fraud/reports/update/:reportId", isAuthenticated, updateFraudReport)
