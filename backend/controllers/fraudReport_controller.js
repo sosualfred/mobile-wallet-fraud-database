e672261f9ac9923b58bc98e7bb9e816c155132ec
Vida Owusu <vidakumiwaaowusu@gmail.com>
2024-08-02 09:24:41 +0000

    resolved conflicts

--- backend/controllers/fraudReport_controller.js
+++ backend/controllers/fraudReport_controller.js
@@ -2,60 +2,72 @@ import { FraudReportModel } from "../models/fraudReport_model.js";
 import { UserModel } from "../models/user_model.js";
 import { fraudReportSchema } from "../schema/fraudReport_schema.js";
 
-export const addFraudReport = async (req, res, next) => {
-  const { error, value } = fraudReportSchema.validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const id = req.session?.user?.id || req?.user?.id;
-
-  const user = await UserModel.findById(id);
-  if (!user) {
-    return res.status(404).send("User not found");
-  }
-
-  const newReport = await FraudReportModel.create({
-    ...value,
-    user: id,
-  });
-
-  user.fraudReport.push(newReport._id);
-
-  await user.save();
-
-  res.status(201).json({
-    message: "Your fraud report has been successfully submitted.",
-  });
-};
-
-
-export const deleteFraudReport = async (req, res, next) => {
+export const addFraudReport=async (req, res, next)=>{
   try {
+    const { error, value } = fraudReportSchema.validate(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+  
     const id = req.session?.user?.id || req?.user?.id;
-    console.log('User ID:', id);
-    console.log('Report ID:', req.params.reportId);
-     // Find the user to ensure they exist
+  
     const user = await UserModel.findById(id);
     if (!user) {
       return res.status(404).send("User not found");
     }
-
-     // Find and delete the report where _id matches and user ID matches
-    const deletedReport = await FraudReportModel.findByIdAndDelete({
-      _id: req.params.reportId,
-      user: id
-    });
- 
-    if (!deletedReport) {
-      return res.status(404).send({ message:"Report not found or you do not have permission to delete this report."});
-    }
   
-    user.fraudReport.pull(req.params.reportId);
+    const newReport = await FraudReportModel.create({
+      ...value,
+      user: id,
+    });
+  
+    user.fraudReport.push(newReport._id);
+  
     await user.save();
   
-    res.status(200).json({
-      message: "Your fraud report has been successfully deleted.",
+    res.status(201).json({
+      message: "Your fraud report has been successfully submitted.",
+    });
+    
+    
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+export const deleteFraudReport = async (req, res, next)=>{
+  try {
+    const id = req.session?.user?.id || req?.user?.id;
+      console.log('User ID:', id);
+      console.log('Report ID:', req.params.reportId);
+       // Find the user to ensure they exist
+      const user = await UserModel.findById(id);
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+  
+       // Find and delete the report where _id matches and user ID matches
+      const deletedReport = await FraudReportModel.findByIdAndDelete({
+        _id: req.params.reportId,
+        user: id
+      });
+   
+      if (!deletedReport) {
+        return res.status(404).send({ message:"Report not found or you do not have permission to delete this report."});
+      }
+    
+      user.fraudReport.pull(req.params.reportId);
+      await user.save();
+    
+      res.status(200).json({
+        message: "Your fraud report has been successfully deleted."});
+      
+    
+  } catch (error) {
+    next(error)
+  }
+}
   
 
 export const updateFraudReport = async (req, res, next) => {
@@ -102,7 +114,6 @@ export const updateFraudReport = async (req, res, next) => {
  };
  
 
-};
 
 
 
