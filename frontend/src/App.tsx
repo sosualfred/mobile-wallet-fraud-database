87c179b2cde09c2e9c7dc6e437e8e27640847dc7
mseve <evelynyaabaidoo@gmail.com>
2024-08-09 10:03:15 +0000

    worked on apikey

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,19 @@
 
-<<<<<<< HEAD
-import Home from "./pages/home"
-import Api from "./pages/apikey"
-
-
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import AccountPage from './pages/accountpage';
->>>>>>> d6d0db38cc9f6ab13a35ab58f15b01c19b03930b
+import ApiKey from "./pages/apikey"
 
 function App() {
   return (
-<<<<<<< HEAD
-
-    <div>
-
-      <Home/>
-      <Api/>
-      
-      
-    </div>
-
-  )
-=======
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path = "/apikey" element= {<ApiKey/>}/>
       </Routes>
     </Router>
   );
->>>>>>> d6d0db38cc9f6ab13a35ab58f15b01c19b03930b
 }
 
 export default App;
