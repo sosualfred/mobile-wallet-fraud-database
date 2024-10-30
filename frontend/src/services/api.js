c5c2314d324d8ed6ae831139f1b28c7bdaf34bd2
Jacinta Akpene Sadzi <jakpesad20@gmail.com>
2024-10-30 09:44:23 +0000

    added the routing tonthe user details

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_BASE_URL;
 
-if (!API_URL) {
+if (!baseURL) {
   console.error("API_URL is not defined in the environment variables");
 }
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
