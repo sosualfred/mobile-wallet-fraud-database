5a899531fa534814093316e551887a1a2c2c4c4d
Jacinta Akpene Sadzi <jakpesad20@gmail.com>
2024-11-20 09:37:04 +0000

    admin

--- frontend-admin/src/services/auth.js
+++ frontend-admin/src/services/auth.js
@@ -1,10 +1,6 @@
 import {apiClient} from  "./config"
 
 
-// registration
-export const apiSignup = async (payload) => {
-    return await apiClient.post("/auth/register",payload)
-}
 // login
 export const apiLogin = async (payload) => {
     return apiClient.post("/api/admin/login",payload)
